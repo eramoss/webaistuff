64141b13d30c8e6d1b52fa2fc0f5c98e9adf2a70
eramoss <edulramos@outlook.com>
2024-06-04 12:26:12 -0300

    dockerize

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :webaistuff, Webaistuff.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "webaistuff_dev",
+  username: System.get_env("POSTGRES_USER") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
+  database: System.get_env("POSTGRES_DB") || "webaistuff_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -23,7 +23,7 @@ config :webaistuff, WebaistuffWeb.Endpoint,
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
-  secret_key_base: "L8wCLQL/17D9jIkolA7Qv9kolSfz+Pt+tAx4WPn9E0LW/iN87ATSArsnd1FLqw7Y",
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   watchers: [
     esbuild: {Esbuild, :install_and_run, [:webaistuff, ~w(--sourcemap=inline --watch)]},
     tailwind: {Tailwind, :install_and_run, [:webaistuff, ~w(--watch)]}
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\webaistuff" eval Webaistuff.Release.migrate
