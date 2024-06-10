28630a621cfacf8362ab6f8804a5528657f71aa0
eramoss <edulramos@outlook.com>
2024-06-10 19:54:36 -0300

    remove unused inspect

--- lib/webaistuff/accounts.ex
+++ lib/webaistuff/accounts.ex
@@ -76,11 +76,9 @@ defmodule Webaistuff.Accounts do
 
   """
   def register_user(attrs, opts \\ [validate_password: true, validate_provider: false]) do
-      user = %User{}
+      %User{}
       |> User.registration_changeset(attrs, opts)
       |> Repo.insert()
-      IO.inspect(user)
-      user
   end
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
