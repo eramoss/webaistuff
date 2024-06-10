0608107f5a6a497c3c111e85c2f10bed45cbb68a
eramoss <edulramos@outlook.com>
2024-06-10 07:52:14 -0300

    remove defaults

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :webaistuff, Webaistuff.Repo,
-  username: System.get_env("POSTGRES_USER") || "postgres",
-  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
-  hostname: System.get_env("POSTGRES_HOST") || "localhost",
-  database: System.get_env("POSTGRES_DB") || "webaistuff_dev",
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD") ,
+  hostname: System.get_env("POSTGRES_HOST"),
+  database: System.get_env("POSTGRES_DB"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
